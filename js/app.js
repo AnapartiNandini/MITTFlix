a460683d33fd27287446bfa35d590b8ce0ddb68c
Nandini Anaparti <nandini.anaparti@gmail.com>
2021-04-18 21:43:51 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,74 +1,114 @@
-let allMovies;
+let api_key = 'a2fabed3f0a936c4ebc2bc4bea53b00c';
+let apikey = "9ba03c1d";
+let titles = document.querySelector('.titles-wrapper');
+let formElement = document.querySelector('#search');
+let inputElement = formElement.querySelector('input');
+let next = document.querySelector('#next');
+let previous = document.querySelector('#previous');
+let page_number = 1;
 
-fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=0b941991fb739be72fed42ae5e2a4891`)
-  .then(resp => resp.json())
-  .then(movies => {
-    console.log(movies)
-    allMovies = movies.results;
-    return fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=0b941991fb739be72fed42ae5e2a4891&language=en-US`)      
-  })
-  .then(resp => resp.json())
-  .then(json => {    
-    usedGenres = makeUniqueGenres(json, allMovies);
-    appendToHTML(usedGenres);
-    console.log(usedGenres);
-  });
-   
+formElement.onsubmit = e => {
+  e.preventDefault();
+  if (inputElement.value.length > 0) {
+    fetch(`http://www.omdbapi.com/?apikey=${apikey}&s=${inputElement.value}`)
+      .then(response => response.json())
+      .then(json => {
+        json.Search.forEach(movie => {
+          titles.textContent = "";
+          previous.hidden = true;
+          next.hidden = true;
 
-function makeUniqueGenres(results, allMovies) {
-  console.log(results)
-  const usedGenres = {}
-  results.genres.forEach(genre => {
-    allMovies.forEach(movie => {        
-      if (movie.genre_ids.includes(genre.id)) {
-        if(usedGenres[genre.name] === undefined) {
-          usedGenres[genre.name] = [movie]
-        } else {
-          usedGenres[genre.name].push(movie);
-        }
-      }
-    });
-  })
-
-  return usedGenres;
+          fetch(`http://www.omdbapi.com/?apikey=${apikey}&i=${movie.imdbID}`)
+            .then(response => response.json())
+            .then(movie => { 
+              let poster = `<img src="${movie.Poster}"/>`;
+              
+              if (movie.Poster === "N/A") {
+                poster = `<picture>
+                  <source type="image/avif" srcset="images/placeholder-movieimage.avif">
+                  <img src="images/placeholder-movieimage.png">
+                </picture>`;
+              }
+               
+              titles.insertAdjacentHTML('afterbegin', `<div class="movie">
+                ${poster}
+                <div class="overlay">
+                  <div class="title">${movie.Title}</div>
+                  <div class="rating">${movie.imdbRating}/10</div>
+                  <div class="plot">
+                    ${movie.Plot}
+                  </div>
+                </div>
+              </div>`);
+          });
+        })
+      });
+  }
 }
 
-function appendToHTML(usedGenres) {
-  const main = document.querySelector('#root');
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=${page_number}`)
+  .then(response => response.json())
+  .then(data => {
+    populatePage(data.results);
+    adjustPagination(data.total_pages);
+  });
 
-  Object.keys(usedGenres).forEach(genreName => {
-    let html = `
-      <div class="titleList">
-        <div class="title">
-          <h1>${genreName}</h1>
-          <div class="titles-wrapper">
-      `;
+next.onclick = function () {
+  ++page_number;
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=${page_number}`)
+    .then(response => response.json())
+    .then(data => {
+      populatePage(data.results);
+      adjustPagination(data.total_pages);
+    });
+}
 
-    usedGenres[genreName].forEach(movie => {
-      html += `
-        <div class="movie">
-        <img
-          src="https://image.tmdb.org/t/p/w500/${movie.backdrop_path}"
-        />
-        <div class="overlay">
-          <div class="title">${movie.title}</div>
-          <div class="rating">${movie.vote_average}/10</div>
-          <div class="plot">
-            ${movie.overview}
-          </div>
-          <div class="listToggle">
-            <div>
-              <i class="fa fa-fw fa-plus"></i>
-              <i class="fa fa-fw fa-check"></i>
-            </div>
-          </div>
+previous.onclick = function () {
+  --page_number;
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=${page_number}`)
+    .then(response => response.json())
+    .then(data => {
+      populatePage(data.results);
+      adjustPagination(data.total_pages);
+    });
+}
+
+function adjustPagination(totalPages) {
+  if (page_number === 1) {
+    previous.hidden = true;
+  } else {
+    previous.hidden = false;
+  }
+
+  if (page_number === totalPages) {
+    next.hidden = true;
+  } else {
+    next.hidden = false;
+  }
+}
+
+function populatePage(movies) {
+  titles.textContent = "";
+  for (let movie of movies) {
+    let poster = `<img src="https://image.tmdb.org/t/p/w500/${movie.backdrop_path}"/>`;
+
+    if (movie.backdrop_path === undefined) {
+      poster = `<picture>
+      <source type="image/avif" srcset="images/placeholder-movieimage.avif">
+      <img src="images/placeholder-movieimage.png">
+    </picture>`;
+    }
+
+    titles.insertAdjacentHTML("afterbegin", `<div class="movie">
+      ${poster}
+      <div class="overlay">
+        <div class="title">${movie.title}</div>
+        <div class="rating">${movie.vote_average}/10</div>
+        <div class="plot">
+          ${movie.overview}
         </div>
       </div>
-      `
-    });
-
-    html += `</div></div></div>`
-
-    main.insertAdjacentHTML('beforeend', html);
-  });
+    </div>`
+    )
+  }
 }
